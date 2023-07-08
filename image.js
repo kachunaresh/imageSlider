6fc551d1acb3605035503f04ee67d08c298b10ef
kachunaresh <138811373+kachunaresh@users.noreply.github.com>
2023-07-08 15:34:45 +0530

    Add files via upload

---
+++ image.js
@@ -0,0 +1,12 @@
+var allImage=document.images;
+console.log(allImage)
+for(i=0;i<allImage.length;i++)
+{
+    allImage[i].src=`./photos/${i}.jpg`
+    allImage[i].addEventListener("click",function(){
+        document.getElementById("bigimage").style.backgroundImage=`url(${this.src})`
+        document.getElementById("mainCont").style.backgroundImage=`url(${this.src})`
+        
+        
+    })
+}
